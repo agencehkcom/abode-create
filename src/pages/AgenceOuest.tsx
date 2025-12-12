5a8db3808425695e3c223d6642cebe3f84ef17ec
AmourEternel <agence.hkcom@gmail.com>
2025-12-12 15:29:50 +0100

    Update AgenceOuest.tsx

--- src/pages/AgenceOuest.tsx
+++ src/pages/AgenceOuest.tsx
@@ -84,7 +84,7 @@ const AgenceOuest = () => {
                   07 44 71 68 75
                 </a>
               </Button>
-              <Button asChild variant="outline" size="lg" className="border-white/30 text-white hover:bg-white/10">
+              <Button asChild size="lg" className="border border-white/30 bg-transparent text-white hover:bg-white/10 hover:text-white">
                 <Link to="/contact">
                   Nous contacter
                   <ArrowRight className="ml-2 h-5 w-5" />
